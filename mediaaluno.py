4de01c73f34465c2f8af7133eae6ceb09584b739
Hudson678 <Hudsonmores31@gmail.com>
2024-09-12 12:57:49 -0300

    aprovado e reprovado

---
+++ mediaaluno.py
@@ -0,0 +1,15 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+nt = float(input("nota do aluno: "))
+
+if nt >= 7:
+    print ("aprovado")
+else:
+        print ("reprovado")
+
+
+
+
+
+
+
+
